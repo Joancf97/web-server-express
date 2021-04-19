4c2e949fe52692f54491b5ae49eb0e06f5836fc5
jose Andres <jcastanedaforno@gmail.com>
2021-04-19 12:43:17 -0600

    =serv html /public

--- src/app.js
+++ src/app.js
@@ -1,32 +1,24 @@
+// Core modules 
+const path = require('path');
 // Funciton of express
 const express = require('express');
 
 // Call the function to create a new express app (instance)
 const app = express();
 
-// Home
-app.get('', (req, res) => {
-  res.send('Hello express!');
-});
+// costumize the server
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath));
 
-// help
-app.get('/help', (req, res) => {
-  res.send('Helo page');
-});
-
-// about
-app.get('/about', (req, res) => {
-  res.send('about page');
-});
-
-// help
+// weather
 app.get('/weather', (req, res) => {
-  res.send('hello weather');
+  res.send({
+    location: "Philadelphia",
+    forecast: "It currently 15 degrees Celcius"
+  });
 });
 
 // server up
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });
-
-
