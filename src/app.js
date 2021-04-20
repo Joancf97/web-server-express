e284aec421d65b564a02618eae9f2fe30658b5b7
jose Andres <jcastanedaforno@gmail.com>
2021-04-20 10:19:49 -0600

    =add partials header and foother

--- src/app.js
+++ src/app.js
@@ -2,38 +2,43 @@
 const path = require('path');
 // Funciton of express
 const express = require('express');
+const hbs = require ('hbs');
 
 // Call the function to create a new express app (instance)
 const app = express();
 // Paths  for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Handlebars - ingect dinamic values into the html
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+// hbs partials 
+hbs.registerPartials(partialsPath);
 // costumize the server - static (pages that are never going to change)
 app.use(express.static(publicDirectoryPath));
 
 // Render home view
 app.get('', (req, res) => {
   res.render('index', { 
-    title: "Weather",
-    name: "Jose Andres"
+    header: "home page",
+    author: "Jose Andres"
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { 
-    title: "about page",
-    name: "Jose Andres"
+    header: "about page",
+    author: "Jose Andres"
   });
 });
 
 app.get('/help', (req, res) => {
   res.render('help', { 
-    title: "help page",
-    phrase: "What's your question?"
+    header: "help page",
+    phrase: "What's your question?",
+    author: "Jose Andres"
   });
 });
 
@@ -41,7 +46,21 @@ app.get('/help', (req, res) => {
 app.get('/weather', (req, res) => {
   res.send({
     location: "Philadelphia",
-    forecast: "It currently 15 degrees Celcius"
+    forecast: "It currently 15 degrees Celcius",
+    author: "Jose Andres"
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', { 
+    errormsj: "Help page not found"
+  });
+});
+
+// default
+app.get('*', (req, res) => {
+  res.render('404', { 
+    errormsj: "Page not found"
   });
 });
 
