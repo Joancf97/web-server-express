8ae23b8437ff15375e3ac982dc164e22093dddfd
jose Andres <jcastanedaforno@gmail.com>
2021-04-23 16:10:31 -0600

    =fetch from client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('client side javascript code');
+
+//  Fetch api from the client side
+fetch('http://localhost:3000/weather?address=Guatemala').then((response) => {
+  response.json().then((data) => {
+    if(data.error){ 
+      console.log(data.error);
+    } else { 
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
